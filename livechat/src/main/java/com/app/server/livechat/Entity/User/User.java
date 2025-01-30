495b6020661f4884c0f8b8716cb909d8c9273497
AlexDev <ariverar151097@icloud.com>
2025-01-29 23:50:15 -0600

    base user entity for backend applications

--- livechat/src/main/java/com/app/server/livechat/Entity/User/User.java
+++ livechat/src/main/java/com/app/server/livechat/Entity/User/User.java
@@ -1,12 +1,18 @@
 package com.app.server.livechat.Entity.User;
 
 import jakarta.persistence.MappedSuperclass;
-
+import lombok.Getter;
+import lombok.ToString;
+@Getter
 @MappedSuperclass
+@ToString
 public class User {
 
     private String email;
     private String password;
+
+    public User() {
+    }
 
     public User(String email, String password) {
         this.email = email;
