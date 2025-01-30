67c222f1cc5c6093a0ae1dcbd2f596c35fd41aca
AlexDev <ariverar151097@icloud.com>
2025-01-29 23:49:01 -0600

    base  dto user for livechat apps

---
+++ livechat/src/main/java/com/app/server/livechat/DTO/LivechatUserDTO.java
@@ -0,0 +1,22 @@
+package com.app.server.livechat.DTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.ToString;
+
+@ToString
+public class LivechatUserDTO {
+
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("token")
+    private String token;
+
+    public LivechatUserDTO(Long id, String token) {
+        this.id = id;
+        this.token = token;
+    }
+
+}
