df8e7d34226e49276c0a4334e4f0c428c37f4c1f
AlexDev <ariverar151097@icloud.com>
2025-02-17 21:26:17 -0600

    to garantize to execute JwtAuthValidatorFilter before UsernamePasswordAuthenticationFilter

--- livechat/src/main/java/com/app/server/livechat/Config/AppConfig.java
+++ livechat/src/main/java/com/app/server/livechat/Config/AppConfig.java
@@ -1,16 +1,23 @@
 package com.app.server.livechat.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.app.server.livechat.Config.Security.JwtAuthValidatorFilter;
 
 @Configuration
 @EnableWebSecurity
 public class AppConfig {
+
+
+    @Autowired
+    private JwtAuthValidatorFilter jwtAuthValidatorFilter;
+
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http)  throws Exception{
@@ -20,6 +27,7 @@ public class AppConfig {
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(jwtAuthValidatorFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
                 
     }
