f63eac374ad1a4bd409c5412b9e0751009a4d71c
AlexDev <ariverar151097@icloud.com>
2025-02-19 21:19:06 -0600

    validate token fn to get claims

--- livechat/src/main/java/com/app/server/livechat/Utils/JwtUtils.java
+++ livechat/src/main/java/com/app/server/livechat/Utils/JwtUtils.java
@@ -8,6 +8,8 @@ import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -28,21 +30,23 @@ public class JwtUtils {
         SecretKey key = Keys.hmacShaKeyFor(decodedKey);
         return Jwts.builder()
             .setClaims(tokenClaims)
+            .setSubject(tokenClaims.get("UserName"))
             .setIssuedAt(new Date(System.currentTimeMillis()))
             .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
             .signWith(key,SignatureAlgorithm.HS256)
             .compact();
     }
 
-    public void  validateToken(String token){
+    public Jws<Claims>  validateToken(String token){
         
         try {
-            Jwts.parserBuilder().setSigningKey(getSecret()).build().parseClaimsJws(token);
-            System.out.println("los claims son" + Jwts.parserBuilder().setSigningKey(getSecret()).build().parseClaimsJws(token).getBody());
+            return Jwts.parserBuilder().setSigningKey(getSecret()).build().parseClaimsJws(token);
 
         } catch (Exception e) {
             System.out.println("error validating Token");
         }
+
+        return null;
         
 
     }
