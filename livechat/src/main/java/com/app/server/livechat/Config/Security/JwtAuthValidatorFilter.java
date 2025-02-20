9f8dc4556f6ef8ffe840291d3f01251becc59c0c
AlexDev <ariverar151097@icloud.com>
2025-02-19 21:25:18 -0600

    createAuthentication method for create a user to set into spring security context

--- livechat/src/main/java/com/app/server/livechat/Config/Security/JwtAuthValidatorFilter.java
+++ livechat/src/main/java/com/app/server/livechat/Config/Security/JwtAuthValidatorFilter.java
@@ -1,11 +1,23 @@
 package com.app.server.livechat.Config.Security;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.app.server.livechat.Utils.JwtUtils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.io.IOException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -29,7 +41,18 @@ public class JwtAuthValidatorFilter extends OncePerRequestFilter {
 
         //TODO : validate the token
         if(jwtToken != null && jwtToken.length() > 0){
-            jwtUtils.validateToken(jwtToken);
+            Jws<Claims> claims = jwtUtils.validateToken(jwtToken);
+
+            if(claims != null){
+                System.out.println("los claims del filter" +claims.getBody());
+                Authentication auth = createAuthentication(claims.getBody());
+                System.out.println("auth es" +auth);
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+                
+
+
+            }
 
         
         }
@@ -37,4 +60,17 @@ public class JwtAuthValidatorFilter extends OncePerRequestFilter {
 
 
     }
+
+    private Authentication createAuthentication(Claims claims) {
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority(claims.get("Role").toString()));
+
+        UserDetails userDetails = new User(claims.getSubject(), "", new ArrayList<>());
+
+        return new UsernamePasswordAuthenticationToken(userDetails, "", authorities);
+    }
+
+    
 }
