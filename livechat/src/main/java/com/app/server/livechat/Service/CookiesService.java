ad383e905abe3bf9056526b7c40e2a614256c357
alexDev1015 <ariverar151097@icloud.com>
2025-03-04 00:21:26 -0600

    create auth cookie for development

---
+++ livechat/src/main/java/com/app/server/livechat/Service/CookiesService.java
@@ -0,0 +1,33 @@
+package com.app.server.livechat.Service;
+
+import java.net.http.HttpHeaders;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Service
+public class CookiesService {
+
+    public void addCookie(String name, String value, HttpServletResponse response){
+
+
+        Cookie authCookie = new Cookie(name, value);
+        authCookie.setHttpOnly(true);
+        authCookie.setAttribute("SameSite", "Lax"); 
+
+        authCookie.setSecure(false);
+        authCookie.setPath("/");
+        authCookie.setMaxAge(60*60*24*7);
+
+        response.addCookie(authCookie);
+
+        
+
+
+    }
+
+    //TODO : Implement the method deleteCookie
+
+}
