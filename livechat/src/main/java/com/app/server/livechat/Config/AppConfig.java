b23d77102f505027e9cbd37257484fd2101e100d
alexDev1015 <ariverar151097@icloud.com>
2025-03-17 23:15:02 -0600

    update appconfig to allow corrs form 4200

--- livechat/src/main/java/com/app/server/livechat/Config/AppConfig.java
+++ livechat/src/main/java/com/app/server/livechat/Config/AppConfig.java
@@ -1,5 +1,7 @@
 package com.app.server.livechat.Config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +9,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.app.server.livechat.Config.Security.JwtAuthValidatorFilter;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
@@ -21,17 +28,31 @@ public class AppConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http)  throws Exception{
-        return http.csrf(c->c.ignoringRequestMatchers("/**").disable())
-
+        return http
+                .csrf(c->c.ignoringRequestMatchers("/**").disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) 
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthValidatorFilter, UsernamePasswordAuthenticationFilter.class)
-                .cors()
-                .and()
                 .build();
                 
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        System.out.println("CORS Configuration");
+        corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:4200"));
+        corsConfiguration.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration); // Apply CORS to all endpoints
+        return source;        
+    }
+
 }
+    
