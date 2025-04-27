eef91089cc26a6683b72b4f853b676e5d9945572
AlexDev <ariverar151097@icloud.com>
2025-04-27 17:59:04 -0600

    get username method request controller

--- livechat/src/main/java/com/app/server/livechat/Controller/LivechatUser/LivechatUserController.java
+++ livechat/src/main/java/com/app/server/livechat/Controller/LivechatUser/LivechatUserController.java
@@ -1,72 +1,67 @@
 package com.app.server.livechat.Controller.LivechatUser;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.app.server.livechat.DTO.LivechatUserDTO;
+import com.app.server.livechat.Entity.Conversation.Conversation;
 import com.app.server.livechat.Entity.User.LivechatUser;
-import com.app.server.livechat.Entity.User.User;
+import com.app.server.livechat.Repository.Conversations.ConversationRepository;
 import com.app.server.livechat.Repository.User.UserRepository;
-import com.app.server.livechat.Service.CookiesService;
 import com.app.server.livechat.Service.LivechatUser.LivechatUserServiceImpl;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import java.net.http.HttpHeaders;
-import java.util.List;
-
-import org.apache.catalina.connector.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 
+@Slf4j
 @RestController
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
-@RequestMapping("/auth")
+@RequestMapping("/user")
 public class LivechatUserController {
-
-    @Autowired
-    LivechatUserServiceImpl livechatUserService;
 
     @Autowired
     UserRepository userRepository;
 
     @Autowired
-    CookiesService cookiesService;
+    ConversationRepository conversationRepository;
 
-    @PostMapping("/login")
-    public ResponseEntity<?> loginRequest(@RequestBody User userFormCredentials, HttpServletResponse  response) {
-        try {
+    @GetMapping("find")
+    public Long findUser(@RequestParam(required = false) String email, @RequestParam(required = false) Long conversationId) {
+        System.out.println("email: " + email);
 
 
-            String responseToken = livechatUserService.login(userFormCredentials.getEmail(), userFormCredentials.getPassword());
-            
-            if(responseToken != null){
-                cookiesService.addCookie("auth-cookie", responseToken, response);
-                    return new ResponseEntity<>(new LivechatUserDTO(responseToken), HttpStatus.OK);
+        try{
+            if(!email.isEmpty()){
+                LivechatUser user =  userRepository.findByEmail(email).get();
+                if(!user.getUserName().isEmpty()){
+                    return user.getId();
+                }
             }
-            else
-            {
-                return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+            else{
+                if(conversationId != null){
+                    Conversation conversation = conversationRepository.findById(conversationId).get();
+                    conversation.getParticipants();
+                }
 
             }
-         
-   
-        } catch (Exception e) {
-            // TODO: handle exception
+
+        }catch (Exception e){
+                return null;
+            }
+        return null;
         }
 
-        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    @GetMapping("getUserName")
+    public ResponseEntity<?> getUsername(@RequestParam Long userId){
+        try{
+            String userName = userRepository.findById(userId).get().getUserName();
+            if(userName != null && !userName.isEmpty()){
+                return new ResponseEntity<>(userName, HttpStatus.OK);
+            }
+            return  new ResponseEntity<>("user", HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            return  new ResponseEntity<>("user", HttpStatus.NOT_FOUND);
+        }
+
+        }
     }
 
-
-    
-}
